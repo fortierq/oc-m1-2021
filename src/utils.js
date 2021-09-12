ad50e6e03c2734bb719239451c4cf4ef066c8ead
Quentin Fortier <qpfortier@gmail.com>
2021-09-12 17:18:12 +0200

    Update glouton

--- src/utils.js
+++
@@ -1,8 +0,0 @@
-prefix = "/oc-m1-2021"
-if (typeof prefix == "undefined") {
-    prefix = ""
-}
-
-function url(relative) {
-    return prefix + "/web/viewer.html?file=" + encodeURI(prefix) + "%2F" + relative;
-}

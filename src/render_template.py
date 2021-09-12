a59b5181949a85ab8755347efc3b36d07978c011
Quentin Fortier <qpfortier@gmail.com>
2021-09-12 19:07:21 +0200

    Add summary generation

--- src/render_template.py
+++ src/render_template.py
@@ -1,12 +1,31 @@
 import jinja2
 from pathlib import Path
+from collections import defaultdict
 
-loader = jinja2.FileSystemLoader('.')
-env = jinja2.Environment(loader=loader)
+env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
 
 template = env.get_template("template_pdf.md")
 
+tds = defaultdict(set)
+cours = {}
+
 for file in Path(".").rglob("*_*/**/*.pdf"):
-    file.with_suffix(".md") \
-        .write_text(template.render(pdf=str(file),
-                                    zoom="page-width" if "TD" in str(file) else "page-fit"))
+    md = file.with_suffix(".md")
+    if "TD" in str(file):
+        tds[file.parts[-2]].add(md)
+    else:
+        cours[file.parts[-2]] = str(md)
+    md.write_text(template.render(pdf=str(file), zoom="page-width" if "TD" in str(file) else "page-fit"))
+
+
+def to_text(filename):
+    return filename.replace("_", " ").title()
+
+
+summary = Path("SUMMARY.md")
+with summary.open("w") as f:
+    for section in cours:
+        i = section.find("_")
+        f.write(f"- [{to_text(section[i+1:])}]({cours[section]})\n")
+        for td in tds[section]:
+            f.write(f"\t- [{to_text(td.stem)}]({td})\n")

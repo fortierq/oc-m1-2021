c2fd11b81919af540315ad1dd9201b63455b260c
Quentin Fortier <qpfortier@gmail.com>
2021-09-15 21:21:43 +0200

    Update render

--- src/render_template.py
+++ src/render_template.py
@@ -26,7 +26,6 @@ summary = Path("SUMMARY.md")
 with summary.open("w") as f:
     f.write("# Summary\n\n")
     for section in cours:
-        i = section.find("_")
-        f.write(f"- [{to_text(section[i+1:])}]({cours[section]})\n")
+        f.write(f"- [{to_text(section)}]({cours[section]})\n")
         for td in tds[section]:
             f.write(f"\t- [{to_text(td.stem)}]({td})\n")

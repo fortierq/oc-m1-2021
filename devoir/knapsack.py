0e930a4ad5770c09ccc17ce2460025d9c688ccbc
Quentin Fortier <qpfortier@gmail.com>
2021-09-27 19:19:20 +0200

    Update knapsack.py

--- devoir/knapsack.py
+++ devoir/knapsack.py
@@ -1,6 +1,6 @@
 def knapsack(capacity, objects):
-  # à remplir
-  # renvoyer la valeur maximum que l'on peut obtenir en prennant des objets dont le poids total est inférieur à capacity
+    # à remplir
+    # renvoyer la valeur maximum que l'on peut obtenir en prennant des objets dont le poids total est inférieur à capacity
   
  
 objects = [(2, 1), (3, 7), (6, 10), (5, 10), (8, 13), (2, 1), (2, 1)] # chaque objet est un couple (poids, valeur)

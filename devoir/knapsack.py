27f955f0ba4ae8dc9d5865179d45ccc18d8d7920
Sourcery AI <>
2021-10-05 17:16:35 +0000

    'Refactored by Sourcery'

--- devoir/knapsack.py
+++ devoir/knapsack.py
@@ -2,21 +2,21 @@
 def knapsack1(C, objects): 
     n = len(objects) + 1 # +1 pour pouvoir accéder à l'élement n, pareil pour C
     v = [ [0]*n for i in range(C+1)]
-    for k in range(1, n) :
-        for c in range(0, C+1) :
+    for k in range(1, n):
+        for c in range(C+1):
             if (c-objects[k-1][0]>=0) :
                 v[c][k] = max(v[c][k-1], (v[c-objects[k-1][0]][k-1] + objects[k-1][1]))
             else :
-                v[c][k] = v[c][k-1]        
+                v[c][k] = v[c][k-1]
     return v[c][k]  
 
 #En stockant uniquement la valeur précedente
 def knapsack2(C, objects): 
-    n = len(objects) + 1 
+    n = len(objects) + 1
     v = [ 0*n for i in range(C+1)]
-    for k in range(1, n) :
+    for k in range(1, n):
         vcopy = v.copy()
-        for c in range(0, C+1) :
+        for c in range(C+1):
             if (c-objects[k-1][0]>=0) :
                 vcopy[c] = max(v[c], (v[c-objects[k-1][0]] + objects[k-1][1]))
         v = vcopy

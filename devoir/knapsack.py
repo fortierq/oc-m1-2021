6f0c0b99670de4481c603c6dd8c26920754bfe89
Quentin Fortier <qpfortier@gmail.com>
2021-10-10 15:05:12 +0200

    Delete knapsack.py

--- devoir/knapsack.py
+++
@@ -1,7 +0,0 @@
-def knapsack(C, objects):
-    # à remplir
-    # renvoyer la valeur maximum que l'on peut obtenir en prennant des objets dont le poids total est inférieur à C
-  
- 
-objects = [(2, 1), (3, 7), (6, 10), (5, 10), (8, 13), (2, 1), (2, 1)] # chaque objet est un couple (poids, valeur)
-print(knapsack(10, objects)) # knapsack(10, objets) doit renvoyer 18
